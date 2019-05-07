5b367b6d4e241827f7d48201f8c750d5005b6959
Aleksandr Matskiv <alexandr.matskiv@gmail.com>
2019-05-07 11:19:15 +0300

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,32 +8,32 @@
                 <div class="col-md-8">
                     <article class="post">
                         <div class="post-thumb">
-                                <div class="text-uppercase text-center">Информация о блоге</div>
-                            </a>
-                        </div>
-                        <div class="post-content">
-                            <header class="entry-header text-center text-uppercase">
+
+                            <div class="post-content">
+                                <header class="entry-header text-center text-uppercase">
+                                    <h3 class="text-uppercase">Информация о блоге</h3>
+                                    <br>
+
+                                </header>
+                                <div class="entry-content">
+                                    Блог создан в учебных целях, для изучения фреймфорка Laravel.
 
 
-                            </header>
-                            <div class="entry-content">
+                                    <div class="btn-continue-reading text-center text-uppercase">
 
+                                    </div>
+                                </div>
+                                <div class="social-share">
 
-                                <div class="btn-continue-reading text-center text-uppercase">
-
+                                    <ul class="text-center pull-right">
+                                        <li><a class="s-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a class="s-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a class="s-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
+                                        <li><a class="s-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+                                        <li><a class="s-instagram" href="#"><i class="fa fa-instagram"></i></a></li>
+                                    </ul>
                                 </div>
                             </div>
-                            <div class="social-share">
-
-                                <ul class="text-center pull-right">
-                                    <li><a class="s-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-                                    <li><a class="s-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-                                    <li><a class="s-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
-                                    <li><a class="s-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
-                                    <li><a class="s-instagram" href="#"><i class="fa fa-instagram"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
                     </article>
                 </div>
                 @include('sidebar')
